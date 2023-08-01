78818a59f2522188d86a04336ad627d421b01aca
Engraya <ahmadyakubuaya@gmail.com>
2023-08-01 18:07:13 +0100

    Template nad Password Reset Commit

---
+++ baseapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+    path('', views.homePage, name='home'),
+    path('signUp', views.signUp, name='register'),
+    path('signIn', views.signIn, name='login'),
+    path('signOut', views.signOut, name='logout'),
+
+
+    #password Reset urls
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="network/passwordManage/password_reset_form.html"), name="reset_password"),
+    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(template_name="network/passwordManage/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="network/passwordManage/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="network/passwordManage/password_reset_complete.html"), name="password_reset_complete")
+]
+
--- baseapp/views.py
+++ baseapp/views.py
@@ -1,4 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import UserRegistrationForm, UserLoginForm
 # Create your views here.
 
+
+def homePage(request):
+    context = {}
+    return render(request, 'baseapp/homePage.html', context)
+
+
+def signUp(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User Registration Succesful...")
+            return redirect("home")
+        else:
+            messages.error(request, "Registration Unsuccessful, Invalid Credentials Entered!!")
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form' : form}
+    return render(request, 'network/registerPage.html', context)
+
+
+def signIn(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, "Congratulations, you are now logged in....")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid Credentials Entered..Try again Later!!")
+
+        else:
+            messages.error(request, "Invalid Username or Password used....Try again Later!!!")
+    else:
+        form = UserLoginForm()
+    context = {'form' : form}
+    return render(request, 'network/loginPage.html', context)
+
+      
+def signOut(request):
+    logout(request)
+    messages.info(request, "Successfully Logged out!!")
+    return redirect("login")
